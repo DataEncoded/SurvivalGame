a28893618b21339ab5eb5d872593a52ac3a1801a
GummyScript <joshmuellerps@gmail.com>
2021-09-01 11:14:15 -0500

    Added rotation functionality

--- src/client/CameraScript.client.lua
+++ src/client/CameraScript.client.lua
@@ -5,7 +5,7 @@ local RunService = game:GetService("RunService")
 local camera = workspace.CurrentCamera -- Camera object
 local player = game.Players.LocalPlayer -- Player objects
 
-local CAMERA_OFFSET = Vector3.new(-1, 90, 0) -- Constant used to set offset of the camera from the player
+local CAMERA_OFFSET = Vector3.new(-1, 30, 0) -- Constant used to set offset of the camera from the player
 
 while camera.CameraSubject == nil do
 	RunService.RenderStepped:Wait()
@@ -15,6 +15,7 @@ camera.CameraType = Enum.CameraType.Scriptable
 
 local function onRenderStep()
     if player.Character then
+		local character = player.Character or player.CharacterAdded:Wait()
 		local playerPosition = player.Character.HumanoidRootPart.Position
 		local cameraPosition = playerPosition + CAMERA_OFFSET
 		
