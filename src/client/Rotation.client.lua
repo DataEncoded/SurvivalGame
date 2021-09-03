aafd51f9f5d3d9e523ee7f4c07b1584ba2192d3b
DataEncoded <dataencoded@gmail.com>
2021-09-02 21:31:09 -0500

    Cleand up rotation code

--- src/client/Rotation.client.lua
+++ src/client/Rotation.client.lua
@@ -1,48 +1,54 @@
---Whole thing really messes, should be cleaned up but im too lazy at this point.
+--Cleaned up :) -WithinCode
 
 local run = game:GetService("RunService")
 local player = game.Players.LocalPlayer
 local mouse = player:GetMouse()
-local camera = game.Workspace.CurrentCamera
+
+--Create a bunch of variables to be changed later on
+local character = nil;
+local align, attachment0, attachment1;
+local humanoidRootPart;
+
+local characterConnection = player.CharacterAdded:Connect(function(c)
+	--Create attachment when new player added
+	character = c;
+	
+	align = Instance.new("AlignOrientation", player:FindFirstChild("HumanoidRootPart"))
+	align.MaxTorque = 10000000
+	align.Responsiveness = 200
+	align.PrimaryAxisOnly = true
+
+	attachment0 = Instance.new("Attachment")
+
+	attachment1 = Instance.new("Attachment")
+
+	align.Attachment0 = attachment0
+	align.Attachment1 = attachment1
+
+
+	humanoidRootPart = character:WaitForChild("HumanoidRootPart")
+
+	--Create attachment parents to where stuff will be moved
+	attachment0.Parent = humanoidRootPart
+	attachment1.Parent = workspace.Terrain
+	align.Parent = humanoidRootPart
+
+
+
+end)
 
 run.RenderStepped:Connect(function()
-	local player = game.Players.LocalPlayer.Character
 
-	if player then
-		local align = player:FindFirstChild("HumanoidRootPart"):FindFirstChild("AlignOrientation")
-		local attachment0 = player:FindFirstChild("HumanoidRootPart"):FindFirstChild("Attachment")
-		local attachment1 = workspace.Terrain:FindFirstChild("Attachment")
+	if player and humanoidRootPart and mouse.Hit then
 
-		if not align then
-			align = Instance.new("AlignOrientation", player:FindFirstChild("HumanoidRootPart"))
-			align.MaxTorque = 10000000
-			align.Responsiveness = 200
-			align.PrimaryAxisOnly = true
-		end
+		--Do calculations for where they are
 
-		if not attachment0 then
-			attachment0 = Instance.new("Attachment", player:FindFirstChild("HumanoidRootPart"))
-		end
+		local mousePos = mouse.Hit.Position
 
-		if not attachment1 then
-			attachment1 = Instance.new("Attachment", workspace.Terrain)
-		end			
+		local finalAngle = CFrame.new(humanoidRootPart.Position, Vector3.new(mousePos.X, humanoidRootPart.Position.Y, mousePos.Z))
 
+		--Update attachment position
 
-		if align.Attachment0 ~= attachment0 then
-			align.Attachment0 = attachment0
-		end
-		if align.Attachment1 ~= attachment1 then
-			align.Attachment1 = attachment1
-		end
-
-		local mouse = game.Players.LocalPlayer:GetMouse()	
-
-		local mousePos = mouse.Hit.Position			
-
-		local finalAngle = CFrame.new(player.HumanoidRootPart.Position, Vector3.new(mousePos.X, player.HumanoidRootPart.Position.Y, mousePos.Z))
-
-		attachment0.WorldCFrame = player:FindFirstChild("HumanoidRootPart").CFrame
 		attachment1.CFrame = finalAngle
 		attachment1.WorldOrientation = Vector3.new(attachment1.WorldOrientation.X, attachment1.WorldOrientation.Y, attachment1.WorldOrientation.Z)
 	end
